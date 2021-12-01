6466d4795da7d1c43e22e826d30fc0e0d3fec641
AthorOrtRam <athorortegaramos@gmail.com>
2021-12-01 13:03:18 +0100

    turtle crossing game
    turtle crossing game

---
+++ scoreboard.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+FONT = ("Courier", 18, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-280, 250)
+        self.refrescar_puntuacion()
+
+    def aumentar_nivel(self):
+        self.level += 1
+        self.refrescar_puntuacion()
+
+    def refrescar_puntuacion(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="center", font=FONT)
